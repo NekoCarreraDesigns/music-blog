e131cee14533e0393eb8e3fa3ebd8888c5380ef6
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-06 10:56:41 -0600

    add more info to pragraph, styled some elements, and added a new header

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ let body = document.body;
 let h1El = document.createElement("h1");
 let h2El = document.createElement("h2");
 let infoEl = document.createElement("div");
+let infoH1El = document.createElement("h1");
 let pEl = document.createElement("p");
 
 h1El.textContent = "You've Never Heard of Them";
 h2El.textContent = "Music blog for music snobs";
+infoH1El.textContent = "Animal Collective";
 pEl.textContent =
-  "Animal Collective makes some weird music, that on so many levels twists and turn. they sound like what I'm sure was what Brian Wilson's LSD laden mind, thoughts sounded like";
+  "Animal Collective makes some weird music, that on so many levels twists and turn. they sound like what I'm sure was what Brian Wilson's LSD laden thoughts sounded like, each song has Layers upon layers of textures and interweaving melodies, often they compliment or collide with one another, the vocals sometimes harmonious, sometimes screamed, and reminiscent of the aforementioned Beach Boy. A comparison they don't care for. In the early days of the groups music the songs were psychedelic landscapes, that transported the listener to the place where they had intended, the era of finger picked guitars, vocal loops, and ambient textures. After this period they introduced more electronic elements such as keyboards, and samples. this led to the more layered sound of the band.";
 
 body.appendChild(h1El);
 body.appendChild(h2El);
 body.appendChild(infoEl);
+infoEl.appendChild(infoH1El);
 infoEl.appendChild(pEl);
 
 h1El.setAttribute(
